5c459d24f4ab891152b57f2f5579dd86a6e8e121
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-09-01 12:54:49 +0200

    feat(main): add CLI entrypoint for crawler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+function main() {
+    const args = process.argv.slice(2);
+    if (args.length !== 1) {
+        console.error('Wrong number of arguments! Usage: npm run start <BASE_URL>');
+        process.exit(1);
+    }
+    const base = args[0].trim();
+    console.log(`Starting crawler at base URL: "${base}"`);
+    process.exit(0);
+}
+
+main();
